bc706bd3d764c0243da440119adfca39aee065ee
jimmy <43307602@qq.com>
2023-11-02 23:47:07 +0800

    20231102

--- src/layouts/admin/components/AdminHeader.vue
+++ src/layouts/admin/components/AdminHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 通过flex指定子元素水平布局 -->
-    <div class="bg-white h-[64px] flex pr-4 border-b border-slate-200">
+    <div class="bg-white h-[64px] flex pr-4 border-b border-slate-100">
         <!-- 左边栏收缩,展开 -->
         <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 hover:bg-gray-200"
             @click="handleMenuWidth">
@@ -11,17 +11,27 @@
         </div>
         <!-- 右边容器 通过ml-auto让其在父容器的右边-->
         <div class="ml-auto flex">
+            <!-- 点击刷新 -->
+            <el-tooltip class="box-item" effect="dark" content="刷新" placment="bottom">
+                <div
+                    class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200" @click="handleRefresh">
+                    <el-icon>
+                        <Refresh v-if="!isFullscreen"/>
+                    </el-icon>
+                </div>
+            </el-tooltip>
             <!-- 点击全屏展示 -->
             <el-tooltip class="box-item" effect="dark" content="全屏" placment="bottom">
                 <div
-                    class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200">
+                    class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200" @click="toggle">
                     <el-icon>
-                        <FullScreen />
+                        <FullScreen v-if="!isFullscreen"/>
+                        <Aim v-else />
                     </el-icon>
                 </div>
             </el-tooltip>
             <!-- 下拉头像 -->
-            <el-dropdown>
+            <el-dropdown class="flex items-center justify-center">
                 <span class="el-dropdown-link flex items-center justify-center text-gray-700 text-xs">
                     <!-- 头像Avatar -->
                     <el-avatar class="mr-2" :size="25"
@@ -44,6 +54,7 @@
 
 <script setup>
 import { useMenuStore } from '@/stores/menu';
+import { useFullscreen } from '@vueuse/core';
 
 //引入了菜单 store
 const menuStore = useMenuStore()
@@ -53,4 +64,9 @@ const handleMenuWidth = () => {
     // 动态设置菜单的宽度大小
     menuStore.handleMenuWidth()
 }
+
+// isFullscreen,toggle 表示当前是否处于全屏,toggle用于动态切换全屏,非全屏
+const {isFullscreen,toggle}=useFullscreen()
+// 刷新页面
+const handleRefresh=()=>location.reload()
 </script>
--- src/layouts/admin/components/AdminTagList.vue
+++ src/layouts/admin/components/AdminTagList.vue
@@ -1,5 +1,144 @@
 <template>
-    <div class="bg-indigo-700" text-white>
-        标签导航栏
+    <div class="fixed top-[64px] h-[44px] px-2 right-0 z-50 flex items-center bg-white transition-all duration-300 shadow"
+        :style="{ left: menuStore.menuWidth }">
+        <!-- 左边:导航栏 -->
+        <el-tabs v-model="activeTab" type="card" class="demo-tabs" @tab-remove="removeTab" style="min-width: 10px;">
+            <el-tab-pane v-for="item in tabList" :key="item.path" :label="item.title" :name="item.path"
+                :closable="item.path != '/admin/index'">
+                {{ item.content }}
+            </el-tab-pane>
+        </el-tabs>
+
+        <!-- 右边:下拉栏 -->
+        <span class="ml-auto flex items-center justify-center h-[32px] w-[32px]">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <el-icon>
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>关闭全部</el-dropdown-item>
+                        <el-dropdown-item>关闭其它</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </span>
     </div>
+    <div class="h-[44px]"></div>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { useMenuStore } from '@/stores/menu'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router';
+
+const menuStore = useMenuStore()
+const route = useRoute()
+// 当前被选中的 tab
+const activeTab = ref(route.path)
+const tabList = ref([
+    {
+        title: '仪表盘',
+        path: "/admin/index"
+    },
+    {
+        title: '文章管理',
+        path: "/admin/article/list"
+    },
+    {
+        title: '分类管理',
+        path: "/admin/category/list"
+    },
+    {
+        title: '标签管理',
+        path: "/admin/tag/list"
+    },
+    {
+        title: '博客设置',
+        path: "/admin/blog/setting"
+    }
+])
+// 添加tab标签页
+function addTab(tab) {
+    // 标签是否不存在
+    let isTableNotExister = tabList.value.findIndex(item => item.path == tab.path) == -1
+    // 如果不存在
+    if (isTableNotExister) {
+        tabList.value.push(tab)
+    }
+}
+
+// 在路由切换前被调用
+onBeforeRouteUpdate((to, from) => {
+    // 设置被激活的tab标签
+    activeTab.value = to.path
+    // 添加tab标签页
+    addTab({ title: to.meta.title, path: to.path })
+})
+const removeTab = (targetName) => {
+
+}
+</script>
+<style>
+.el-tabs__item {
+    font-size: 12px;
+    border: 1px solid #d8dce5 !important;
+    border-radius: 3px !important;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item {
+    margin-left: 0.1rem !important;
+    margin-right: 0.1rem !important;
+}
+
+.el-tabs__item.is-active {
+    background-color: var(--el-color-primary) !important;
+    color: #fff;
+}
+
+.el-tabs__item.is-active::before {
+    content: "";
+    background-color: #fff;
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    position: relative;
+    margin-right: 4px;
+}
+
+.el-tabs {
+    height: 32px;
+}
+
+.el-tabs__header {
+    margin-bottom: 0;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__nav {
+    border: 0;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item {
+    height: 32px;
+    line-height: 32px;
+    border: 0;
+    background: #fff;
+}
+
+.el-tabs--card>.el-tabs__header {
+    border: 0;
+}
+
+.el-tabs__nav-prev,
+.el-tabs__nav-next {
+    line-height: 35px;
+}
+
+.is-disabled {
+    cursor: not-allowed;
+    color: #d1d5db;
+}
+</style>
+  
